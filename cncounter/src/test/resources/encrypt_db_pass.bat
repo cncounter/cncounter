7818c7f4852d4892bc1eb277f838fc31c20269b8
renfufei <renfufei@qq.com>
2014-09-15 21:05:42 +0800

    提示Druid数据源需要对数据库密码进行解密

--- cncounter/src/test/resources/encrypt_db_pass.bat
+++ cncounter/src/test/resources/encrypt_db_pass.bat
@@ -3,4 +3,4 @@
 @rem cd 的目的是跳到有druid-xxx.jar的 lib 目录执行
 @echo 正在生成Druid使用的加密密码......
 cd ..\lib
-java -cp druid-1.0.7.jar com.alibaba.druid.filter.config.ConfigTools eoNRNBgRk397mVyu66MHYuZDsepCeZ8A > ..\classes\pass.txt
+java -cp druid-1.0.7.jar com.alibaba.druid.filter.config.ConfigTools eoNRNBgRk397mVyu66MHYuZDsepCeZ8A	> ..\classes\pass.txt
