e62b7046efbeeb529d17699b385d493b1cfa44f0
renfufei <renfufei@qq.com>
2015-05-14 15:18:00 +0800

    搬迁服务器

--- cncounter/src/test/resources/sql/01_cncounter.sql
+++ cncounter/src/test/resources/sql/01_cncounter.sql
@@ -1,10 +1,10 @@
 -- 创建数据库 -- 授权
 /*
-CREATE DATABASE `eReqDlGXyFUaXwyfjjVg` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `eReqDlGXyFUaXwyfjjVg`;
-DROP USER '6EhSiGpsmSMRyZieglUImkTr';
-CREATE USER '6EhSiGpsmSMRyZieglUImkTr'@'%' IDENTIFIED BY 'eoNRNBgRk397mVyu66MHYuZDsepCeZ8A';
-grant all privileges on eReqDlGXyFUaXwyfjjVg.* to '6EhSiGpsmSMRyZieglUImkTr'@'%' identified by 'eoNRNBgRk397mVyu66MHYuZDsepCeZ8A';
+CREATE DATABASE `cncounter` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `cncounter`;
+-- DROP USER 'cncounter';
+CREATE USER 'cncounter'@'%' IDENTIFIED BY 'cncounter';
+grant all privileges on cncounter.* to 'cncounter'@'%' identified by 'cncounter';
 flush privileges;
 
 */
