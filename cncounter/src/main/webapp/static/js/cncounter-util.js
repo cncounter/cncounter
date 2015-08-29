85e9acfb1fd5a3d313f3b61cfdb91831f9595f98
renfufei <renfufei@qq.com>
2015-08-29 18:15:46 +0800

    增加工具方法

--- cncounter/src/main/webapp/static/js/cncounter-util.js
+++ cncounter/src/main/webapp/static/js/cncounter-util.js
@@ -4,6 +4,30 @@ function refreshPage(){
 	location.reload();
 };
 
+
+//////////////////////////////////////////////////////////////////////////////////////
+///////// 工具函数
+//////////////////////////////////////////////////////////////////////////////////////
+
+// 调试
+function debug(obj) {
+	try{
+		// 只适用于具有console的浏览器
+		if(!window["console"] || !window["console"]["dir"] || !window["console"]["info"]){  return;	}
+		var params = Array.prototype.slice.call(arguments, 0);
+		for(var i=0; i < params.length; i++){
+			if ("object" === typeof params[i] ) {
+				window["console"]["dir"](params[i]);
+			} else {
+				window["console"]["info"](params[i]);
+			}
+		}
+	} catch (ex){
+		// 吃掉异常
+	}
+};
+
+
 // 解析JSON字符串为对象
 function parseJSON2Object(text){
 	var object = null;
@@ -58,6 +82,72 @@ function requestAjax(url, data, type, successCallback, errorCallback, isParseJSO
 	    }
 	});
 };
+
+
+/****************************************************
+ *
+ *			cookie相关的函数
+ *
+ ****************************************************/
+
+// 获取cookie值(key)
+function getCookie(cookieName){
+	//获取cookie字符串
+	var strCookie=document.cookie;
+	//将多cookie切割为多个名/值对
+	var arrCookie=strCookie.split("; ");
+	var cookieValue = null;
+	//遍历cookie数组,处理每个cookie对
+	for(var i=0;i<arrCookie.length;i++){
+		var arr=arrCookie[i].split("=");
+		//找到cookie,并返回它的值
+		if(cookieName==arr[0]){
+			cookieValue=unescape(arr[1]);
+			break;
+		}
+	}
+	//
+	if(!cookieValue){
+		cookieValue = "";
+	}
+	cookieValue = decodeURIComponent(cookieValue);
+	//
+	return cookieValue;
+};
+
+// 设置cookie值(key,value)
+function setCookie(cookieName, cookieValue, expiredays, domain){
+	// 0 比较特殊
+	if(0 === cookieValue){
+		cookieValue = 0;
+	} else if(!cookieValue){
+		cookieValue = "";
+	}
+	// 编码
+	cookieValue = encodeURIComponent(cookieValue);
+	//获取cookie字符串
+	var cookieStr= cookieName + "=" + cookieValue;
+
+    // 过期时间
+    if(expiredays && !isNaN(expiredays)){
+        var exdate=new Date();
+        exdate.setDate(exdate.getDate()+expiredays);
+        cookieStr += "; expires="+exdate.toGMTString();
+    }
+    // 域名
+    //domain = domain || document.domain;
+    if(domain){
+        cookieStr += "; path=" + "/";
+        cookieStr += "; domain="+domain;
+    }
+
+	// 保存本地 cookie
+	document.cookie = cookieStr;
+
+	// 返回设置后的值
+	return cookieValue;
+};
+
 
 // Date 转 str
 function date2Str(d){
