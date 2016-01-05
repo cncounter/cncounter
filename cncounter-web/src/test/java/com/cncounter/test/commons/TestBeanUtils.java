6e1271781f068b2f8cda245bfd7078b5746a9206
renfufei <renfufei@qq.com>
2016-01-05 15:51:01 +0800

    测试类

--- cncounter-web/src/test/java/com/cncounter/test/commons/TestBeanUtils.java
+++ cncounter-web/src/test/java/com/cncounter/test/commons/TestBeanUtils.java
@@ -2,6 +2,7 @@ package com.cncounter.test.commons;
 
 import com.cncounter.cncounter.model.user.User;
 import com.cncounter.util.common.CNC;
+import org.springframework.beans.BeanUtils;
 
 /**
  * Created by Administrator on 2016/1/5.
@@ -21,6 +22,7 @@ public class TestBeanUtils {
             System.out.println("user.equals(u2): " + user.equals(u2));
             System.out.println("user.getUsername(): " + user.getUsername());
             System.out.println("user2.getUsername(): " + user2.getUsername());
+            System.out.println("----------------------------------------");
         }
 
         User user3 = new User();
@@ -28,5 +30,13 @@ public class TestBeanUtils {
         System.out.println("user.equals(user3): " + user.equals(user3));
         System.out.println("user.getPassword(): " + user.getPassword());
         System.out.println("user3.getPassword(): " + user3.getPassword());
+        System.out.println("----------------------------------------");
+
+        //
+        User user4 = new User();
+        BeanUtils.copyProperties(user, user4);
+        System.out.println("user.equals(user4): " + user.equals(user4));
+        System.out.println("user.getPassword(): " + user.getPassword());
+        System.out.println("user4.getPassword(): " + user4.getPassword());
     }
 }
