c4c2910f183a226f5f507808befc337229f21501
renfufei <renfufei@qq.com>
2016-08-17 16:48:22 +0800

    增加部分RedisBaseDAO处理方法

--- cncounter-web/src/main/java/com/cncounter/cncounter/dao/redis/api/RedisBaseDAO.java
+++ cncounter-web/src/main/java/com/cncounter/cncounter/dao/redis/api/RedisBaseDAO.java
@@ -1,6 +1,7 @@
 package com.cncounter.cncounter.dao.redis.api;
 
 import java.io.Serializable;
+import java.util.Set;
 
 /**
  * Redis 基础数据DAO
@@ -13,20 +14,49 @@ public interface RedisBaseDAO {
 	 * @return
 	 */
 	public boolean saveObject(String key, Serializable value);
+
+	/**
+	 * 保存自动过期对象
+	 * @param key 键
+	 * @param value 值
+	 * @param seconds 过期时间
+	 * @return
+	 */
+	public boolean saveObject(String key, Serializable value, int seconds);
+
+	/**
+	 * 如果不存在则设置
+	 * @param key 键
+	 * @param value 值
+	 * @param seconds 超时时间
+	 * @return
+	 */
+	public boolean setNX(String key, Serializable value, int seconds);
 	/**
 	 * 获取对象
 	 * @param key 键
 	 * @return
 	 */
 	public Object getObject(String key);
-	
+
+	// 获取对应类型的 Cache
+	public <T> T getObject(String key, Class<T> clazz);
+
 	/**
 	 * 删除
 	 * @param key 键
 	 * @return
 	 */
 	public boolean deleteByKey(String key);
-	
+
+	/**
+	 * 设置过期时间
+	 * @param key
+	 * @param seconds
+	 * @return
+	 */
+	public boolean expire(String key, int seconds);
+
 	/**
 	 * 设置值
 	 * @param key 键
@@ -67,4 +97,24 @@ public interface RedisBaseDAO {
 	 * @return
 	 */
 	public String getString(String key);
+
+	/**
+	 * 获取cache的key
+	 * @param pattern 模式 xxx*
+	 * @return
+	 */
+	public Set<String> keys(String pattern);
+
+	/**
+	 * 查询过期时间
+	 * @param key
+	 * @return
+	 */
+	public int ttl(String key);
+	/**
+	 * 查询过期时间
+	 * @param key
+	 * @return
+	 */
+	public int getExpireSeconds(String key);
 }
--- cncounter-web/src/main/java/com/cncounter/cncounter/dao/redis/impl/RedisBaseImpl.java
+++ cncounter-web/src/main/java/com/cncounter/cncounter/dao/redis/impl/RedisBaseImpl.java
@@ -1,36 +1,32 @@
 package com.cncounter.cncounter.dao.redis.impl;
 
-import java.io.Serializable;
-import java.nio.charset.Charset;
-
+import com.cncounter.cncounter.dao.redis.api.RedisBaseDAO;
+import com.cncounter.util.string.StringNumberUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Repository;
 
-import com.cncounter.cncounter.dao.redis.api.RedisBaseDAO;
+import java.io.Serializable;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Repository
 public class RedisBaseImpl implements RedisBaseDAO {
 	/**
 	 * 前缀字符串
 	 */
-	public static String PRE_FIX = "cncounter:RedisBaseImpl:";
+	public static String PRE_FIX = "";
+	/**
+	 * 默认cache过期时间,秒 =
+	 */
+	public static Long DEFAULT_EXPIRED_SECONDS = TimeUnit.DAYS.toSeconds(10) ;
 	//
 	public static String BYTE_CODE = "UTF-8";
 	// 模板
 	@Autowired
 	protected RedisTemplate<String, Serializable> redisTemplate;
-	// 序列号工具
-	protected StringRedisSerializer serializer = new StringRedisSerializer(Charset.forName(BYTE_CODE));
-
-	// 获取字节数组
-	public byte[] getByteArray(String string){
-		string = String.valueOf(string);
-		byte[] byteArray = serializer.serialize(string);
-		//
-		return byteArray;
-	}
 
 	/**
 	 * 给KEY加上前缀
@@ -45,7 +41,24 @@ public class RedisBaseImpl implements RedisBaseDAO {
 		//
 		String _key = prefixKey(key);
 		//
-		redisTemplate.delete(_key);
+		try{
+			redisTemplate.delete(_key);
+		} catch (Exception e){
+			redisTemplate.delete(_key);
+		}
+		return true;
+	}
+
+	@Override
+	public boolean expire(String key, int seconds) {
+		//
+		String _key = prefixKey(key);
+		//
+		try{
+			redisTemplate.expire(_key, seconds, TimeUnit.SECONDS);
+		} catch (Exception e){
+			redisTemplate.expire(_key, seconds, TimeUnit.SECONDS);
+		}
 		return true;
 	}
 
@@ -53,6 +66,9 @@ public class RedisBaseImpl implements RedisBaseDAO {
 	public Long getLong(String key) {
 		//
 		String value = getString(key);
+		if(!StringNumberUtil.isLong(value)){
+			return null;
+		}
 		return Long.valueOf(value);
 	}
 
@@ -62,14 +78,37 @@ public class RedisBaseImpl implements RedisBaseDAO {
 		Object value = getObject(key);
 		return String.valueOf(value);
 	}
-	
+
 	@Override
 	public Object getObject(String key) {
 		//
 		String _key = prefixKey(key);
 		//
-		Object value = redisTemplate.opsForValue().get(_key);
+		Object value = null;
+		try{
+			value = redisTemplate.opsForValue().get(_key);
+		} catch (Exception e){
+			value = redisTemplate.opsForValue().get(_key);
+		}
 		return value;
+	}
+
+	@Override
+	public <T> T getObject(String key, Class<T> clazz) {
+		Object cacheVO = null;
+		try {
+			cacheVO = getObject(key);
+		} catch (Throwable ex){
+			ex.printStackTrace();
+		}
+		if(clazz.isInstance(cacheVO)){
+			try {
+				return (T) cacheVO;
+			} catch (Throwable e){
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 
 	@Override
@@ -87,14 +126,64 @@ public class RedisBaseImpl implements RedisBaseDAO {
 		//
 		return incrLong(key, value);
 	}
-	
+
 	@Override
 	public boolean saveObject(String key, Serializable value) {
+		int seconds = DEFAULT_EXPIRED_SECONDS.intValue();
+		boolean result = saveObject(key, value, seconds);
+		return result;
+	}
+	@Override
+	public boolean saveObject(String key, Serializable value, int seconds) {
+		boolean result = pureSaveObject(key, value);
+		expire(key, seconds);
+		return result;
+	}
+
+	public boolean pureSaveObject(String key, Serializable value) {
 		//
 		String _key = prefixKey(key);
 		//
-		redisTemplate.opsForValue().set(_key, value);
+		try{
+			redisTemplate.opsForValue().set(_key, value);
+		} catch (Exception e){
+			redisTemplate.opsForValue().set(_key, value);
+		}
 		return true;
+	}
+
+
+	@Override
+	public boolean setNX(String key, Serializable value, int seconds) {
+
+		RedisSerializer keySerializer = redisTemplate.getKeySerializer();
+		RedisSerializer valueSerializer = redisTemplate.getValueSerializer();
+
+		byte[] keyBytes = keySerializer.serialize(key);
+		byte[] valueBytes = valueSerializer.serialize(value);
+		Boolean result = false;
+		// 设置
+		// setNX
+		// 获取连接。 归还
+		RedisConnection redisConnection = null;
+		try {
+			redisConnection = redisTemplate.getConnectionFactory().getConnection();
+		} catch (Throwable ex){
+			redisConnection = redisTemplate.getConnectionFactory().getConnection();
+		}
+		try{
+			result = redisConnection.setNX(keyBytes, valueBytes);
+		} catch (Throwable ex){
+			result = redisConnection.setNX(keyBytes, valueBytes);
+		} finally {
+			if(null != redisConnection){
+				redisConnection.close();
+			}
+		}
+		if(result){
+			expire(key, seconds);
+		}
+		return result;
 	}
 
 	@Override
@@ -102,7 +191,11 @@ public class RedisBaseImpl implements RedisBaseDAO {
 		//
 		String _key = prefixKey(key);
 		//
-		redisTemplate.opsForValue().set(_key, value);
+		try{
+			redisTemplate.opsForValue().set(_key, value);
+		} catch (Throwable ex){
+			redisTemplate.opsForValue().set(_key, value);
+		}
 		return value;
 	}
 
@@ -111,8 +204,47 @@ public class RedisBaseImpl implements RedisBaseDAO {
 		//
 		String _key = prefixKey(key);
 		//
-		redisTemplate.opsForValue().set(_key, value);
+		try{
+			redisTemplate.opsForValue().set(_key, value);
+		} catch (Throwable ex){
+			redisTemplate.opsForValue().set(_key, value);
+		}
 		return true;
 	}
 
+	/**
+	 * 获取cache的key
+	 *
+	 * @param pattern 模式 xxx*
+	 * @return
+	 */
+	@Override
+	public Set<String> keys(String pattern) {
+		Set<String> keys = null;
+		try{
+			keys = redisTemplate.keys(pattern);
+		} catch (Throwable ex){
+			keys = redisTemplate.keys(pattern);
+		}
+		return keys;
+	}
+
+	public int ttl(String key){
+		return getExpireSeconds(key);
+	}
+	public int getExpireSeconds(String key){
+		Long result = -1L;
+		try{
+			result = redisTemplate.getExpire(key, TimeUnit.SECONDS);
+		} catch (Throwable ex){
+			result = redisTemplate.getExpire(key, TimeUnit.SECONDS);
+		}
+		//
+		if(null == result){
+			result = -1L;
+		}
+		//
+		return result.intValue();
+	}
 }
+
