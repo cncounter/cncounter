a20a4f9323bffc0da73c1404a2a6e7924234e9da
renfufei <renfufei@qq.com>
2016-12-03 21:40:32 +0800

    修正MySQL数据库授权脚本

--- 服务器config/SQL_NoSQL/sql/01_cncounter.sql
+++ 服务器config/SQL_NoSQL/sql/01_cncounter.sql
@@ -6,8 +6,8 @@ USE `cncounter`;
 CREATE USER 'cncounter'@'%' IDENTIFIED BY 'cncounter';
 grant all privileges on cncounter.* to 'cncounter'@'%' identified by 'cncounter';
 grant all privileges on cncounter.* to cncounter@localhost identified by 'cncounter';
-grant all privileges on cncounter.* to 'cncounter@cnc-web1' identified by 'cncounter';
-grant all privileges on cncounter.* to 'cncounter@cnc-web2' identified by 'cncounter';
+grant all privileges on cncounter.* to 'cncounter'@'cnc-web1' identified by 'cncounter';
+grant all privileges on cncounter.* to 'cncounter'@'cnc-web2' identified by 'cncounter';
 flush privileges;
 
 */
